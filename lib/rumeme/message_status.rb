6ccac096774c5dd89d559cd35f0f2861a99307c2
Anatoliy Plastinin <anatoliy.plastinin@gmail.com>
2013-05-12 21:25:20 +0400

    minor cleanup

--- lib/rumeme/message_status.rb
+++ lib/rumeme/message_status.rb
@@ -1,7 +1,7 @@
 module Rumeme
   # This class defines constants used to specify a message delivery status.
   class MessageStatus
-    NONE =  0
+    NONE = 0
     PENDING = 1
     DELIVERED = 2
     FAILED = 3
