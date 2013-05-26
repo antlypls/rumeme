b6cb77615efe1283b0eded07f5bc30d006914e2e
Anatoliy Plastinin <anatoliy.plastinin@gmail.com>
2013-05-26 10:33:55 +0400

    refactor defaults

--- lib/rumeme/sms_message.rb
+++ lib/rumeme/sms_message.rb
@@ -3,21 +3,35 @@ module Rumeme
   class SmsMessage
     attr_reader :phone_number, :message, :message_id, :delay, :validity_period, :delivery_report
 
-    # Constructor.
-    def initialize args
-      # this defaults must be moved to global configuration
-      defaults = {:phone_number => nil, :message => nil, :message_id => 0, :delay => 0, :validity_period => ValidityPeriod::THREE_DAYS, :delivery_report => false}
-      params = defaults.merge args
-      defaults.keys.each {|key| instance_variable_set("@#{key.to_s}".to_sym, params[key])}
+    # this defaults must be moved to global configuration
+    DEFAULTS = {
+      :phone_number => nil,
+      :message => nil,
+      :message_id => 0,
+      :delay => 0,
+      :validity_period => ValidityPeriod::THREE_DAYS,
+      :delivery_report => false
+    }.freeze
 
-      raise ArgumentError.new("phone_number is empty") if @phone_number.nil? || @phone_number.empty?
-      raise ArgumentError.new("message is empty") if @message.nil? || @message.empty?
+    # Constructor.
+    def initialize(args)
+      params = DEFAULTS.merge(args)
+      DEFAULTS.keys.each {|key| instance_variable_set(:"@#{key}", params[key])}
+
+      check_attributes
 
       @message = @message.gsub("\n",'\n').gsub("\r",'\r').gsub("\\",'\\\\')
     end
 
     def post_string
       "#{@message_id} #{@phone_number} #{@delay} #{@validity_period} #{@delivery_report ? 1 : 0} #{@message}\r\n"
+    end
+
+    private
+
+    def check_attributes
+      raise ArgumentError.new("phone_number is empty") if @phone_number.blank?
+      raise ArgumentError.new("message is empty") if @message.blank?
     end
   end
 end
